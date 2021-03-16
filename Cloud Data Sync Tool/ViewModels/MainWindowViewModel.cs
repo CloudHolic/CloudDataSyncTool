4432c57c279ebf686bdc2aaf8bb6e13961daef97
Cloudholic <bluegkstjr@kaist.ac.kr>
2021-03-16 16:05:24 +0900

    Casing 수정

--- Cloud Data Sync Tool/ViewModels/MainWindowViewModel.cs
+++ Cloud Data Sync Tool/ViewModels/MainWindowViewModel.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
